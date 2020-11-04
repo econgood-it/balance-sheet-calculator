3e1c024afa17cb27eb0336a1fa3ae25df9bd716a
Michael Rudolph <michael.rudolph@ecogood.org>
2020-11-04 15:44:53 +0100

    Add unique index to countryCode column

--- src/migrations/1594223075365-CreateRegionTable.ts
+++ src/migrations/1594223075365-CreateRegionTable.ts
@@ -4,7 +4,7 @@ export class CreateRegionTable1594223075365 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
 
-        const query = `CREATE TABLE IF NOT EXISTS "region" (
+        let query = `CREATE TABLE IF NOT EXISTS "region" (
             "id" SERIAL NOT NULL, 
             "pppIndex" double precision NOT NULL, 
             "countryCode" character varying NOT NULL, 
@@ -12,10 +12,14 @@ export class CreateRegionTable1594223075365 implements MigrationInterface {
             CONSTRAINT "PK_region" PRIMARY KEY ("id")
         )`;
         await queryRunner.query(query);
+        query = `CREATE UNIQUE INDEX "IDX_region_country_code" ON "region" ("countryCode")`;
+        await queryRunner.query(query);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        const query = `DROP TABLE IF EXISTS region`
+        let query = `DROP INDEX IF EXISTS "IDX_region_country_code"`;
+        await queryRunner.query(query);
+        query = `DROP TABLE IF EXISTS region`
         await queryRunner.query(query);
     }
 
