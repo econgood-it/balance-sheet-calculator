33d22193b3c30e3cf8c94008a4bf93bb0383d7f3
Michael Rudolph <michael.rudolph@ecogood.org>
2020-11-04 15:49:25 +0100

    Add TopicWeightCalculator

---
+++ src/calculations/topic.weigt.calculator.ts
@@ -0,0 +1,32 @@
+import {Precalculations} from "./precalculator";
+
+
+export class TopicWeigtCalculator {
+
+    public async calcTopicWeight(topicShortName: string, precalculations: Precalculations): Promise<number> {
+        let weight: number = 1;
+        switch (topicShortName) {
+            case 'A1':
+                weight = await this.constantWeight();
+                break;
+            case 'A2':
+                weight = await this.constantWeight();
+                break;
+            case 'A3':
+                weight = await this.calculateTopicWeightOfA3(precalculations);
+                break;
+            default:
+                weight = 1;
+                break;
+        }
+        return weight;
+    }
+
+    public constantWeight(): number {
+        return 1.0;
+    }
+
+    public calculateTopicWeightOfA3(precalculations: Precalculations): number {
+        throw new Error();
+    }
+}
