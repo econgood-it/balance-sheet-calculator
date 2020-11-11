195c10720db04579d6ac6498eada4118fa87064f
Michael Rudolph <michael.rudolph@ecogood.org>
2020-11-11 15:01:16 +0100

    Log message in case of 500 error

--- src/exceptions/InternalServerException.ts
+++ src/exceptions/InternalServerException.ts
@@ -1,7 +1,9 @@
 import HttpException from "./HttpException";
+import {LoggingService} from "../logging";
 
 class InternalServerException extends HttpException {
     constructor(msg: string) {
+        LoggingService.error(msg);
         super(500, msg);
     }
 }
