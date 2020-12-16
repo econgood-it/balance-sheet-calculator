8b8eed53cfc9f0e8d4d8b2760d4ca3190d7913ca
Michael Rudolph <michael.rudolph@ecogood.org>
2020-12-16 14:51:54 +0100

    Add missing test

---
+++ tests/entities/company.facts.spec.ts
@@ -0,0 +1,48 @@
+import { DatabaseConnectionCreator } from '../../src/database.connection.creator';
+import { Connection, Repository } from "typeorm";
+import { ConfigurationReader } from "../../src/configuration.reader";
+import {IndustrySector} from "../../src/entities/industry.sector";
+import {CompanyFacts} from "../../src/entities/companyFacts";
+import {EmptyCompanyFacts} from "../testData/company.facts";
+
+describe('Company Facts entity', () => {
+
+    let companyFactsRepository: Repository<CompanyFacts>;
+    let connection: Connection;
+
+    beforeAll(async (done) => {
+        connection = await DatabaseConnectionCreator.createConnectionAndRunMigrations(ConfigurationReader.read());
+        companyFactsRepository = connection.getRepository(CompanyFacts);
+        done();
+    });
+
+    afterAll(async (done) => {
+        await connection.close();
+        done();
+    })
+
+    describe('is saved and has the property ', () => {
+        let companyFacts: CompanyFacts;
+        beforeEach(() => {
+            companyFacts = EmptyCompanyFacts;
+        });
+
+        it('profit', async (done) => {
+            companyFacts.profit = 200;
+            const result = await companyFactsRepository.save(companyFacts);
+            expect(result.profit).toBe(200);
+            await companyFactsRepository.remove(result);
+            done();
+        })
+
+        it('financialAssetsAndCashBalance', async (done) => {
+            companyFacts.financialAssetsAndCashBalance = 300;
+            const result = await companyFactsRepository.save(companyFacts);
+            expect(result.financialAssetsAndCashBalance).toBe(300);
+            await companyFactsRepository.remove(result);
+            done();
+        })
+    });
+
+
+})
