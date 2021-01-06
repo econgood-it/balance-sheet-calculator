8d201aef393a0060e671762021804415da1678ea
Michael Rudolph <michael.rudolph@ecogood.org>
2021-01-06 12:24:41 +0100

    Add EmptyCompanyFacts as json to tests

--- tests/testData/company.facts.ts
+++ tests/testData/company.facts.ts
@@ -19,6 +19,22 @@ const employeesFractions: EmployeesFraction[] = [
 export const EmptyCompanyFacts = new CompanyFacts(undefined, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0,[], [], [])
 
+export const EmptyCompanyFactsJson = {
+    totalPurchaseFromSuppliers: 0,
+    totalStaffCosts: 0,
+    profit: 0,
+    financialCosts: 0,
+    incomeFromFinancialInvestments: 0,
+    additionsToFixedAssets: 0,
+    turnover: 0,
+    totalAssets: 0,
+    financialAssetsAndCashBalance: 0,
+    totalSales: 0,
+    supplyFractions: [],
+    employeesFractions: [],
+    industrySectors: [],
+}
+
 export const CompanyFacts1 = new CompanyFacts(undefined, 10000, 900, 500,
   600, 700, 800, 0, 30, 40, 0,
     supplyFractions, employeesFractions, [])
