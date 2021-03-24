4d00e638685420b38acae60d5448a800793470c5
Michael Rudolph <michael.rudolph@ecogood.org>
2021-03-24 10:59:32 +0100

    Add balance sheet type full to migration

--- src/migrations/1616573753595-ReplaceTopicNamesByI18nKeys.ts
+++ src/migrations/1616573753595-ReplaceTopicNamesByI18nKeys.ts
@@ -1,15 +1,25 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 export class ReplaceTopicNamesByI18nKeys1616573753595 implements MigrationInterface {
 
-  private updateQuery(shortName: string, updatedValue: string): string {
-    const from = 'FROM balance_sheet b, rating r';
-    const where = `where b.type = 'Compact' and b."ratingId" = r.id and topic."ratingId" = r.id and topic."shortName"='${shortName}'`
-    return `UPDATE topic SET name='${updatedValue}' ${from} ${where};`;
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await this.replaceTopicNameByI18Keys('v5:compact', 'Compact', queryRunner);
+    await this.replaceTopicNameByI18Keys('v5:full', 'Full', queryRunner);
   }
 
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    const i18nPrefix = 'v5:compact'
-    const shortNameToTranslationKeyMap = new Map([
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const query = `ALTER TABLE "industry_sector" ALTER COLUMN "description" set data type character varying USING description::json->>'en';`
+    await queryRunner.query(query);
+  }
+
+  private async replaceTopicNameByI18Keys(i18nPrefix: string, balanceSheetType: string, queryRunner: QueryRunner) {
+    const shortNameToTranslationKeyMap = this.createTranslationMap(i18nPrefix);
+    for (let [shortName, translationKey] of shortNameToTranslationKeyMap) {
+      await queryRunner.query(this.updateQuery(shortName, translationKey, balanceSheetType));
+    }
+  }
+
+  private createTranslationMap(i18nPrefix: string) {
+    return new Map([
       ['A1', `${i18nPrefix}.A1`],
       ['A2', `${i18nPrefix}.A2`],
       ['A3', `${i18nPrefix}.A3`],
@@ -30,15 +40,17 @@ export class ReplaceTopicNamesByI18nKeys1616573753595 implements MigrationInterf
       ['E2', `${i18nPrefix}.E2`],
       ['E3', `${i18nPrefix}.E3`],
       ['E4', `${i18nPrefix}.E4`],
-    ])
-    for (let [shortName, translationKey] of shortNameToTranslationKeyMap) {
-      await queryRunner.query(this.updateQuery(shortName, translationKey));
-    }
+    ]);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    const query = `ALTER TABLE "industry_sector" ALTER COLUMN "description" set data type character varying USING description::json->>'en';`
-    await queryRunner.query(query);
+  private updateQuery(shortName: string, updatedValue: string, balanceSheetType: string): string {
+    const from = 'FROM balance_sheet b, rating r';
+    const where = `where b.type = '${balanceSheetType}' and b."ratingId" = r.id and topic."ratingId" = r.id and topic."shortName"='${shortName}'`
+    return `UPDATE topic SET name='${updatedValue}' ${from} ${where};`;
   }
+
+
+
+
 
 }
