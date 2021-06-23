37a79251de2e2d7c1b5e4fd74047b2badec606c9
Michael Rudolph <michaelrudolph@posteo.net>
2021-06-23 18:17:06 +0200

    Merge provided ratings with default rating

--- src/dto/create/balance.sheet.create.dto.ts
+++ src/dto/create/balance.sheet.create.dto.ts
@@ -10,30 +10,48 @@ import {
 } from '../../entities/enums';
 import { ValidateNested } from 'class-validator';
 import { Translations } from '../../entities/Translations';
+import { RatingDTO } from '../createAndUpdate/rating.dto';
+import { RatingsDTO } from '../createAndUpdate/ratings.dto';
+import { RatingWithDtoMerger } from '../../merge/rating.with.dto.merger';
 
 export class BalanceSheetDTOCreate {
   @ValidateNested()
   public readonly companyFacts: CompanyFactsDTOCreate;
 
+  @ValidateNested()
+  public readonly rating: RatingDTO | undefined;
+
   public constructor(
     public readonly type: BalanceSheetType,
     public readonly version: BalanceSheetVersion,
-    companyFacts: CompanyFactsDTOCreate
+    companyFacts: CompanyFactsDTOCreate,
+    rating: RatingDTO | undefined
   ) {
     this.companyFacts = companyFacts;
+    this.rating = rating;
   }
 
-  public static readonly fromJSON = strictObjectMapper((accessor) => {
-    return new BalanceSheetDTOCreate(
-      accessor.get('type', balanceSheetTypeFromJSON),
-      accessor.get('version', balanceSheetVersionFromJSON),
-      accessor.getOptional(
-        'companyFacts',
-        CompanyFactsDTOCreate.fromJSON,
-        CompanyFactsDTOCreate.fromJSON({})
-      )
-    );
-  });
+  public static readonly fromJSON = (json: any) => {
+    let rating: RatingDTO | undefined;
+    if ('ratings' in json) {
+      rating = RatingsDTO.fromJSON({ ratings: json.ratings }).toRatingDTO();
+      delete json.ratings;
+    }
+    // 'ratings' in json ? RatingsDTO.fromJSON(json).toRatingDTO() : undefined;
+    const jsonParser = strictObjectMapper((accessor) => {
+      return new BalanceSheetDTOCreate(
+        accessor.get('type', balanceSheetTypeFromJSON),
+        accessor.get('version', balanceSheetVersionFromJSON),
+        accessor.getOptional(
+          'companyFacts',
+          CompanyFactsDTOCreate.fromJSON,
+          CompanyFactsDTOCreate.fromJSON({})
+        ),
+        rating
+      );
+    });
+    return jsonParser(json);
+  };
 
   public async toBalanceSheet(
     language: keyof Translations
@@ -42,6 +60,10 @@ export class BalanceSheetDTOCreate {
       this.type,
       this.version
     );
+    if (this.rating !== undefined) {
+      const ratingWithDtoMerger = new RatingWithDtoMerger();
+      ratingWithDtoMerger.mergeRating(rating, this.rating, this.type);
+    }
     return new BalanceSheet(
       undefined,
       this.type,
