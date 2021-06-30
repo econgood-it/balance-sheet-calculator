1938830db666bd01038b191cc5625a19c698d91b
Michael Rudolph <michaelrudolph@posteo.net>
2021-06-30 15:35:16 +0200

    Remove logging from internal server exception

--- src/exceptions/internal.server.exception.ts
+++ src/exceptions/internal.server.exception.ts
@@ -1,9 +1,7 @@
 import HttpException from './http.exception';
-import { LoggingService } from '../logging';
 
 class InternalServerException extends HttpException {
   constructor(msg: string) {
-    LoggingService.error(msg);
     super(500, msg);
   }
 }
