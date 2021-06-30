c7f1a98e83922fde0e211d2bae81816aa78a40e3
Michael Rudolph <michaelrudolph@posteo.net>
2021-06-30 11:34:35 +0200

    Remove unused validation annotations

--- src/dto/createAndUpdate/topic.or.aspect.dto.ts
+++ src/dto/createAndUpdate/topic.or.aspect.dto.ts
@@ -1,20 +1,12 @@
 import {
-  strictObjectMapper,
-  expectString,
   expectNumber,
+  expectString,
+  strictObjectMapper,
 } from '@daniel-faber/json-ts';
-import { IsIn, IsInt, IsOptional, Max, Min } from 'class-validator';
-import { WEIGHT_VALUES } from '../validation.constants';
 
 export class TopicOrAspectDTO {
-  @IsOptional()
-  @IsIn(WEIGHT_VALUES)
   public weight: number | undefined;
 
-  @IsInt()
-  @Min(-200)
-  @Max(10)
-  @IsOptional()
   public readonly estimations: number | undefined;
 
   public readonly isAspect: boolean;
