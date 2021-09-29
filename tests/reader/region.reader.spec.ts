03f34a94d4f9a729463b2738bec50f0fddb0f440
Michael Rudolph <michaelrudolph@posteo.net>
2021-09-29 15:24:09 +0200

    Add test to read 5_0_6 regions file

--- tests/reader/region.reader.spec.ts
+++ tests/reader/region.reader.spec.ts
@@ -3,11 +3,11 @@ import { Region } from '../../src/entities/region';
 import path from 'path';
 describe('Region reader', () => {
   const readerFolder = '../../src/files/reader';
-  it.only('should read region.csv', async () => {
+  it('should read region.csv', async () => {
     const regionReader = new RegionReader();
     const pathToCsv = path.join(
       path.resolve(__dirname, readerFolder),
-      'regions.csv'
+      'regions_5_0_4.csv'
     );
     const regions: Region[] = await regionReader.read(pathToCsv);
     expect(regions).toContainEqual({
@@ -36,23 +36,23 @@ describe('Region reader', () => {
       path.resolve(__dirname, readerFolder),
       'regions_5_0_6.csv'
     );
-    const regions: Region[] = await regionReader.read(pathToCsv);
+    const regions: Region[] = await regionReader.read(pathToCsv, [22, 242]);
     expect(regions).toContainEqual({
       countryCode: 'ABW',
       countryName: 'Aruba',
-      pppIndex: 1.9628994497935313,
-      ituc: 3.4210526315789473,
+      pppIndex: 1.53937701809995,
+      ituc: 3.52,
     });
     expect(regions).toContainEqual({
       countryCode: 'ISL',
       countryName: 'Iceland',
-      pppIndex: 0.8592191515177231,
+      pppIndex: 1.09979988644301,
       ituc: 1,
     });
     expect(regions).toContainEqual({
       countryCode: 'ZWE',
       countryName: 'Zimbabwe',
-      pppIndex: 3.0325616327831186,
+      pppIndex: 1.41117750678159,
       ituc: 5,
     });
   });
