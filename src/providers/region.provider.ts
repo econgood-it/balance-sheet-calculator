7743cc294cd5c13da233ba642cb1320696b52000
Michael Rudolph <michaelrudolph@posteo.net>
2021-10-13 15:33:40 +0200

    Handle versioning in region provider

--- src/providers/region.provider.ts
+++ src/providers/region.provider.ts
@@ -2,36 +2,45 @@ import { CompanyFacts } from '../entities/companyFacts';
 import { Repository } from 'typeorm';
 import Provider from './provider';
 import { Region } from '../entities/region';
+import { BalanceSheetVersion } from '../entities/enums';
+import NotFoundException from '../exceptions/not.found.exception';
+import { compare, lte } from '@mr42/version-comparator/dist/version.comparator';
 
 export class RegionProvider extends Provider<string, Region> {
   public static createFromCompanyFacts = async (
     companyFacts: CompanyFacts,
-    regionRepository: Repository<Region>
+    regionRepository: Repository<Region>,
+    balanceSheetVersion: BalanceSheetVersion
   ) => {
     const regionProvider = new Provider<string, Region>();
-    for (const supplyFraction of companyFacts.supplyFractions) {
+    const countryCodes = companyFacts.getAllCountryCodes(true);
+    for (const countryCode of countryCodes) {
       regionProvider.set(
-        supplyFraction.countryCode,
-        await regionRepository.findOneOrFail({
-          countryCode: supplyFraction.countryCode,
-        })
+        countryCode,
+        RegionProvider.findRegionByVersionOrFail(
+          await regionRepository.find({
+            countryCode: countryCode,
+          }),
+          balanceSheetVersion
+        )
       );
     }
-
-    for (const employeesFraction of companyFacts.employeesFractions) {
-      regionProvider.set(
-        employeesFraction.countryCode,
-        await regionRepository.findOneOrFail({
-          countryCode: employeesFraction.countryCode,
-        })
-      );
-    }
-    regionProvider.set(
-      companyFacts.mainOriginOfOtherSuppliers.countryCode,
-      await regionRepository.findOneOrFail({
-        countryCode: companyFacts.mainOriginOfOtherSuppliers.countryCode,
-      })
-    );
     return regionProvider;
   };
+
+  private static findRegionByVersionOrFail(
+    regions: Region[],
+    version: BalanceSheetVersion
+  ) {
+    const regionsFiltered = regions.filter((r) =>
+      lte(r.validFromVersion, version)
+    );
+    const regionsFilteredWithDescendingVersion = regionsFiltered.sort(
+      (a: Region, b: Region) => compare(b.validFromVersion, a.validFromVersion)
+    );
+    if (regionsFilteredWithDescendingVersion.length >= 0) {
+      return regionsFilteredWithDescendingVersion[0];
+    }
+    throw new NotFoundException(`Region not found`);
+  }
 }
