926d3eae5bff84a0866599cab9d8a70ad09eb05e
Michael Rudolph <michaelrudolph@posteo.net>
2021-10-20 11:51:16 +0200

    Correct handling of non existing regions

--- src/providers/region.provider.ts
+++ src/providers/region.provider.ts
@@ -3,8 +3,8 @@ import { Repository } from 'typeorm';
 import Provider from './provider';
 import { Region } from '../entities/region';
 import { BalanceSheetVersion } from '../entities/enums';
-import NotFoundException from '../exceptions/not.found.exception';
 import { compare, lte } from '@mr42/version-comparator/dist/version.comparator';
+import NotFoundException from '../exceptions/not.found.exception';
 
 export class RegionProvider extends Provider<string, Region> {
   public static createFromCompanyFacts = async (
@@ -15,32 +15,34 @@ export class RegionProvider extends Provider<string, Region> {
     const regionProvider = new Provider<string, Region>();
     const countryCodes = companyFacts.getAllCountryCodes(true);
     for (const countryCode of countryCodes) {
-      regionProvider.set(
-        countryCode,
-        RegionProvider.findRegionByVersionOrFail(
-          await regionRepository.find({
-            countryCode: countryCode,
-          }),
-          balanceSheetVersion
-        )
+      const foundRegion = RegionProvider.findRegionByVersion(
+        await regionRepository.find({
+          countryCode: countryCode,
+        }),
+        balanceSheetVersion
       );
+      if (foundRegion) {
+        regionProvider.set(countryCode, foundRegion);
+      } else {
+        throw new NotFoundException(`Region ${countryCode} not found`);
+      }
     }
     return regionProvider;
   };
 
-  private static findRegionByVersionOrFail(
+  private static findRegionByVersion(
     regions: Region[],
     version: BalanceSheetVersion
-  ) {
+  ): Region | undefined {
     const regionsFiltered = regions.filter((r) =>
       lte(r.validFromVersion, version)
     );
     const regionsFilteredWithDescendingVersion = regionsFiltered.sort(
       (a: Region, b: Region) => compare(b.validFromVersion, a.validFromVersion)
     );
-    if (regionsFilteredWithDescendingVersion.length >= 0) {
+    if (regionsFilteredWithDescendingVersion.length > 0) {
       return regionsFilteredWithDescendingVersion[0];
     }
-    throw new NotFoundException(`Region not found`);
+    return undefined;
   }
 }
