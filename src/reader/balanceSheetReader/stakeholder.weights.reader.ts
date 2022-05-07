c9c3c00f076a3220e79de797b388c9329f80ac29
Michael Rudolph <michaelrudolph@posteo.net>
2022-05-07 21:41:08 +0200

    Use undefined instead of Option

--- src/reader/balanceSheetReader/stakeholder.weights.reader.ts
+++ src/reader/balanceSheetReader/stakeholder.weights.reader.ts
@@ -1,27 +1,24 @@
 import { Workbook } from 'exceljs';
 import { CellReader } from './cell.reader';
-import { none, Option, some } from '../../calculations/option';
 import Provider from '../../providers/provider';
 
 export class StakeholderWeightsReader {
-  public readFromWorkbook(wb: Workbook): Option<Provider<string, number>> {
+  public readFromWorkbook(wb: Workbook): Provider<string, number> | undefined {
     const cr = new CellReader();
     const weightingSheet = wb.getWorksheet('9. Weighting');
 
     if (!weightingSheet) {
-      return none();
+      return undefined;
     }
 
     const column = 'K';
 
-    return some(
-      new Provider<string, number>([
-        ['A', cr.read(weightingSheet, 49, column).number],
-        ['B', cr.read(weightingSheet, 50, column).number],
-        ['C', cr.read(weightingSheet, 51, column).number],
-        ['D', cr.read(weightingSheet, 52, column).number],
-        ['E', cr.read(weightingSheet, 53, column).number],
-      ])
-    );
+    return new Provider<string, number>([
+      ['A', cr.read(weightingSheet, 49, column).number],
+      ['B', cr.read(weightingSheet, 50, column).number],
+      ['C', cr.read(weightingSheet, 51, column).number],
+      ['D', cr.read(weightingSheet, 52, column).number],
+      ['E', cr.read(weightingSheet, 53, column).number],
+    ]);
   }
 }
