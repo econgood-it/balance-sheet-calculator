76bf75684ef2602616c7751117a0fe041d914c4b
Michael Rudolph <michaelrudolph@posteo.net>
2024-05-31 10:13:17 +0200

    feat: Add 5.09 support for compact balance sheet

--- tests/factories/ratings.factory.spec.ts
+++ tests/factories/ratings.factory.spec.ts
@@ -5,6 +5,27 @@ import {
 import { makeRatingFactory } from '../../src/factories/rating.factory';
 
 describe('Ratings factory', () => {
+  it('should create default ratings for a full balance sheet of version 5.09', () => {
+    const ratings = makeRatingFactory().createDefaultRatings(
+      BalanceSheetType.Full,
+      BalanceSheetVersion.v5_0_9
+    );
+    expect(ratings[0]).toMatchObject({
+      estimations: 0,
+      isWeightSelectedByUser: false,
+      name: 'Human dignity in the supply chain',
+      shortName: 'A1',
+      weight: 1,
+    });
+    expect(ratings[1]).toMatchObject({
+      estimations: 0,
+      isPositive: true,
+      isWeightSelectedByUser: false,
+      name: 'Working conditions and social impact in the supply chain',
+      shortName: 'A1.1',
+      weight: 1,
+    });
+  });
   it('should create default ratings for a full balance sheet', () => {
     const ratings = makeRatingFactory().createDefaultRatings(
       BalanceSheetType.Full,
@@ -31,6 +52,44 @@ describe('Ratings factory', () => {
       isWeightSelectedByUser: false,
       name: 'Negative aspect: violation of human dignity in the supply chain',
       shortName: 'A1.2',
+      weight: 1,
+    });
+  });
+
+  it('should create a default ratings for a compact balance sheet of version 5.09', () => {
+    const ratings = makeRatingFactory().createDefaultRatings(
+      BalanceSheetType.Compact,
+      BalanceSheetVersion.v5_0_9
+    );
+    expect(ratings[0]).toMatchObject({
+      estimations: 0,
+      isWeightSelectedByUser: false,
+      name: 'Human dignity in the supply chain',
+      shortName: 'A1',
+      weight: 1,
+    });
+    expect(ratings[1]).toMatchObject({
+      estimations: 0,
+      isPositive: true,
+      isWeightSelectedByUser: false,
+      name: 'Human dignity in the supply chain',
+      shortName: 'A1.1',
+      weight: 1,
+    });
+    expect(ratings[2]).toMatchObject({
+      estimations: 0,
+      isPositive: false,
+      isWeightSelectedByUser: false,
+      name: 'Negative aspect: violation of human dignity in the supply chain',
+      shortName: 'A1.2',
+      weight: 1,
+    });
+    expect(ratings[ratings.length - 1]).toMatchObject({
+      estimations: 0,
+      isPositive: false,
+      isWeightSelectedByUser: false,
+      name: 'Negative aspect: lack of transparency and wilful misinformation',
+      shortName: 'E4.2',
       weight: 1,
     });
   });
