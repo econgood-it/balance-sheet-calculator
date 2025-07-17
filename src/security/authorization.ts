19a48c07436c8f036c05f885cf698f7f957f0c19
Gerrit Elbrink <leisure.b@gmail.com>
2025-07-17 17:30:03 +0200

    added peer and audit to checkIfCurrentUserIsMember function

--- src/security/authorization.ts
+++ src/security/authorization.ts
@@ -3,6 +3,7 @@ import { NoAccessError } from '../exceptions/no.access.error';
 import { Organization } from '../models/organization';
 import { BalanceSheet } from '../models/balance.sheet';
 import { IOrganizationRepo } from '../repositories/organization.repo';
+import { Role } from '../models/user';
 
 export async function checkIfCurrentUserHasEditorPermissions(
   request: Request,
@@ -25,7 +26,9 @@ export function checkIfCurrentUserIsMember(
   if (
     !(
       request.authenticatedUser &&
-      organization.hasMember(request.authenticatedUser)
+      ( organization.hasMember(request.authenticatedUser)
+      || request.authenticatedUser.role === Role.Peer
+      || request.authenticatedUser.role === Role.Auditor ) 
     )
   ) {
     throw new NoAccessError();
